a44b38ff14782b7b35c9269e95fccd87181c2c56
Tang Yiwu <yiwutang@creditease.cn>
2019-03-19 21:17:39 +0800

    submit solution of lowestCommonAncestor

---
+++ src/com/algorithm/leetcode/medium/LeetCode236_LowestCommonAncestor.java
@@ -0,0 +1,12 @@
+package com.algorithm.leetcode.medium;
+
+import com.algorithm.model.TreeNode;
+
+public class LeetCode236_LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        return left == null ? right : right == null ? left : root;
+    }
+}
