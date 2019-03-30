afaae82493c1418b3bf6cd7e3f2c0ba24ea5fe13
yiwu tang <448143401@qq.com>
2019-03-30 22:58:51 +0800

    动态规划实现leetcode5

--- src/com/algorithm/leetcode/medium/LeetCode5_LongestPalindromicSubstring.java
+++ src/com/algorithm/leetcode/medium/LeetCode5_LongestPalindromicSubstring.java
@@ -36,9 +36,35 @@ public class LeetCode5_LongestPalindromicSubstring {
         return len;
     }
 
+    /**
+     * 动态规划
+     */
+    public String longestPalindrome2(String s) {
+        if (s == null || s.isEmpty()) {
+            return s;
+        }
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        int start = 0;
+        int end = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = (j - i == 0) || (j - i == 1) || (j >= i + 2 && dp[i + 1][j - 1]);
+                }
+                if (dp[i][j] && j - i > end - start) {
+                    start = i;
+                    end = j;
+                }
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
     public static void main(String[] args) {
         LeetCode5_LongestPalindromicSubstring palindromicSubstring = new LeetCode5_LongestPalindromicSubstring();
         System.out.println(palindromicSubstring.longestPalindrome("babad"));
+        System.out.println(palindromicSubstring.longestPalindrome2("babad"));
         System.out.println(palindromicSubstring.longestPalindrome("cbbd"));
+        System.out.println(palindromicSubstring.longestPalindrome2("cbbd"));
     }
 }
