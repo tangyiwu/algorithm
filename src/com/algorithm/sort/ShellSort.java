c90506203ca5eec5e4d9010da4e72ef4f3a2df67
yiwu tang <448143401@qq.com>
2019-04-09 14:18:38 +0800

    shell sort

--- src/com/algorithm/sort/ShellSort.java
+++ src/com/algorithm/sort/ShellSort.java
@@ -1,30 +1,41 @@
 package com.algorithm.sort;
 
+import com.algorithm.utils.ArrayUtil;
+
 public class ShellSort {
     public void sort(int[] a) {
         if (a == null || a.length <= 0) {
             return;
         }
-        int tmp = 0;
-        int incre = a.length;
+        int d = a.length;
         while (true) {
-            incre /= 2;
-            for (int k = 0; k < incre; k++) {
-                for (int i = k + incre; i < a.length; i += incre) {
-                    for (int j = i; j > k; j -= incre) {
-                        if (a[j] > a[j - incre]) {
-                            tmp = a[j - incre];
-                            a[j - incre] = a[j];
-                            a[j] = tmp;
-                        } else {
-                            break;
+            d /= 2;
+            for (int i = 0; i < d; i++) {
+                for (int j = i + d; j < a.length; j += d) {
+                    //插入排序
+                    for (int k = j; k >= d; k -= d) {
+                        if (a[k] < a[k - d]) {
+                            swap(a, k, k - d);
                         }
                     }
                 }
             }
-            if (incre == 1) {
+            if (d == 1) {
                 break;
             }
         }
+    }
+
+    private void swap(int[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        ShellSort shellSort = new ShellSort();
+        int[] a = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+        shellSort.sort(a);
+        ArrayUtil.printArray(a);
     }
 }
