fc5432df5bed79c27922b90058c1ccc70da694d0
yiwu tang <448143401@qq.com>
2021-08-09 22:46:26 +0800

    修改选择排序和插入排序

--- src/com/algorithm/sort/InsertSort.java
+++ src/com/algorithm/sort/InsertSort.java
@@ -11,7 +11,7 @@ public class InsertSort {
         for (int i = 0; i < a.length - 1; i++) {
             for (int j = i + 1; j > 0; j--) {
                 if (a[j] < a[j - 1]) {
-                    SortUtil.swap(a, j, j-1);
+                    SortUtil.swap(a, j - 1, j);
                 }
             }
         }
--- src/com/algorithm/sort/SelectSort.java
+++ src/com/algorithm/sort/SelectSort.java
@@ -11,12 +11,12 @@ public class SelectSort {
         for (int i = 0; i < a.length - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j < a.length; j++) {
-                if (a[j] < a[minIndex]) {
+                if (a[minIndex] > a[j]) {
                     minIndex = j;
                 }
             }
             if (minIndex != i) {
-                SortUtil.swap(a, minIndex, i);
+                SortUtil.swap(a, i, minIndex);
             }
         }
     }
